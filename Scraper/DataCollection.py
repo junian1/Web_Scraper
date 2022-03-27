6219b8489cda3ebb0f4a0ae6400e611276121ec1
junian1 <junianlim3@gmail.com>
2022-03-27 23:56:21 +0800

    commit collection

--- Scraper/DataCollection.py
+++ Scraper/DataCollection.py
@@ -1,6 +1,6 @@
 import Scrape as sp
 import pandas as pd
 
-df = sp.get_jobs("data scientist", 5, False)
+df = sp.get_jobs("data analyst", 1000, False)
 df.to_csv("glassdoor_jobs.csv", index=False)
 
