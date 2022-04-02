26fe89f40e478834fafce098a3a047c120832c11
junian1 <junianlim3@gmail.com>
2022-04-02 17:37:15 +0800

    Update data collection

--- Scraper/DataCollection.py
+++ Scraper/DataCollection.py
@@ -2,6 +2,6 @@ import Scraper as sp
 import pandas as pd
 
 # Can only do a maximum of 1000 at a time due to glassdoor limit
-df = sp.get_jobs("data scientist specialist", 1000, False)
+df = sp.get_jobs("data analyst", 1000, False)
 df.to_csv("glassdoor_jobs.csv", index=False)
 
